596a793103f579d95833c3d6706a78c5d0980a05
dheena <dheena@mericaninc.com>
2023-07-07 11:39:45 +0530

    image uploaded

--- Components/Register/register.jsx
+++ Components/Register/register.jsx
@@ -1,23 +1,49 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { app } from "@/utils/firebase";
+import { app, db, storage } from "@/utils/firebase";
 import { useRouter } from "next/navigation";
+import { ref, uploadBytes } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 
 export default function RegisterPage(){
     const router = useRouter()
+    const [username,setUsername] = useState("")
     const [email,setEmail]=useState("")
     const [password,setPassword]=useState("")
     const [confirmPassword,setConfirmPassword]=useState("")
+    const [file,setFile] = useState("")
+    const [submit, setSubmit]=useState(false)
+    const [urls, setUrls]=useState("")
     const auth = getAuth(app)
-    
+    const savePost =async()=>{
+      await setDoc(doc(db,"data",Date.now().toString()),{username:username,email:email, password:password,confirmPassword:confirmPassword,image:urls})
 
-    const handleSubmit = (e)=>{
+    }
+
+    useEffect(()=>{
+      if(submit===true){savePost()}
+    },[submit])
+
+    const handleSubmit = async(e)=>{
         e.preventDefault();
-        if(password===confirmPassword){
-            alert("password mismatch")
-        }
+        // if(password===confirmPassword){
+        //     alert("password mismatch")
+        // }
+        const storageRef = ref(storage, 'Photos/'+file?.name);
+        uploadBytes(storageRef, file).then((snapshot) => {
+            console.log('Uploaded a blob or file!');
+          }).then(resp=>{
+            getDownloadURL(storageRef).then(async(url)=>{
+                
+                setUrls((values)=>({url}));          
+                setSubmit(true);
+    
+            }) 
+          })
+
+        
         createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed in 
@@ -44,6 +70,24 @@ export default function RegisterPage(){
                   Create and account
                 </h1>
                 <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
+                <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Username
+                    </label>
+                    <input
+                      type="name"
+                      name="name"
+                      id="name"
+                      onChange={e=>setUsername(e.target.value)}
+                      
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="name@company.com"
+                      required=""
+                    />
+                  </div>
                   <div>
                     <label
                       htmlFor="email"
@@ -90,6 +134,23 @@ export default function RegisterPage(){
                       name="confirm-password"
                       id="confirm-password"
                       onChange={e=>setConfirmPassword(e.target.value)}
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Upload Photo
+                    </label>
+                    <input
+                      type="file"
+                      name="file"
+                      id="file"
+                      onChange={e=>setFile(e.target.value)}
                       placeholder="••••••••"
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       required=""
