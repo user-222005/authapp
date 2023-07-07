73b3a3a2d82fe5c68dd8338b662afb6424f0ad4d
dheena <dheena@mericaninc.com>
2023-07-07 16:12:57 +0530

    remove submit

--- Components/Register/register.jsx
+++ Components/Register/register.jsx
@@ -37,7 +37,7 @@ export default function RegisterPage(){
       {
       savePost()
     }
-    },[submit]);
+    },[fetchBusinesses]);
 
     const handleSubmit = async(e)=>{
         e.preventDefault();
