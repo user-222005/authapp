fde0c1f9e6a6de99ac5c8b8f7ca101179a7110b2
dheena <dheena@mericaninc.com>
2023-07-19 12:29:23 +0530

    added todo

--- components/userprofilePage/userprofile.jsx
+++ components/userprofilePage/userprofile.jsx
@@ -102,9 +102,9 @@ const UserProfile = () => {
                           <button
                             className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                             type="button"
-                            onClick={handleClick}
+                            onClick={()=>router.push("/profile/todo")}
                           >
-                            LogOut
+                            Todo
                           </button>
                         </div>
                       </div>
