dd315cd57ae507ecca5c0a97e1dfdb1930804d2e
dheena <dheena@mericaninc.com>
2023-07-19 12:24:04 +0530

    added button

--- components/userprofilePage/userprofile.jsx
+++ components/userprofilePage/userprofile.jsx
@@ -2,7 +2,7 @@
 import { app, db } from '@/utils/firebase'
 import { data } from 'autoprefixer'
 import Image from 'next/image'
-import { getAuth } from 'firebase/auth'
+import { getAuth,signOut } from 'firebase/auth'
 import { collection, getDocs, query, where } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
@@ -102,8 +102,9 @@ const UserProfile = () => {
                           <button
                             className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                             type="button"
+                            onClick={handleClick}
                           >
-                            Connect
+                            LogOut
                           </button>
                         </div>
                       </div>
