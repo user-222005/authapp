d3b3f201369fe711b243c24cf79cf7e5532d61d1
dheena <dheena@mericaninc.com>
2023-07-19 12:34:51 +0530

    added margin

--- components/userprofilePage/userprofile.jsx
+++ components/userprofilePage/userprofile.jsx
@@ -47,7 +47,7 @@ const UserProfile = () => {
 
 
   return (
-    <div className='flex items-center justify-center p-24 mt-32'>
+    <div className='flex items-center justify-center p-32 mt-32'>
       {val.map((item,key)=>{
         return (
           <>
